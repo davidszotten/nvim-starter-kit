074026b8149e4ae036c516c111051a71d6929748
David Szotten <davidszotten@gmail.com>
2024-03-12 10:45:37 +0000

    find-site-packages mini plugin

--- .config/nvim/lua/core/mini-plugins.lua
+++ .config/nvim/lua/core/mini-plugins.lua
@@ -8,15 +8,24 @@ function CopyFilename(with_lines)
     vim.fn.setreg("*", path)
 end
 
-
 function CloseAllSavedBuffers()
     local last_buffer = vim.fn.bufnr('$')
 
-    for n = 1,last_buffer do
+    for n = 1, last_buffer do
         if vim.fn.buflisted(n) and vim.fn.getbufvar(n, '&modified') == 0
         then
             vim.api.nvim_buf_delete(n, {})
         end
     end
+end
 
+function FindSitePackages()
+    local py_cmd = 'import sysconfig; print(sysconfig.get_path(\\"purelib\\"))'
+    -- local virtualenv = os.execute('python -c "' .. py_cmd .. '"')
+    local virtualenv = vim.fn.system('python -c "' .. py_cmd .. '"')
+    return string.gsub(virtualenv, "\n", "") .. "/"
+end
+
+function FormatJson()
+    vim.fn.execute("%!python -m json.tool")
 end
