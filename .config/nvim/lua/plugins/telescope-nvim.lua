d5cd65763dd5a305449bda843497746b04d34718
David Szotten <davidszotten@gmail.com>
2024-03-12 10:48:25 +0000

    telescope dont skip all hidden files, only gitignore/git

--- .config/nvim/lua/plugins/telescope-nvim.lua
+++ .config/nvim/lua/plugins/telescope-nvim.lua
@@ -1,33 +1,39 @@
 -- Fuzzy finder
 return {
-  -- https://github.com/nvim-telescope/telescope.nvim
-  'nvim-telescope/telescope.nvim',
-  lazy = true,
-  branch = '0.1.x',
-  dependencies = {
-    -- https://github.com/nvim-lua/plenary.nvim
-    { 'nvim-lua/plenary.nvim' },
-    {
-      -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
-      cond = function()
-        return vim.fn.executable 'make' == 1
-      end,
+    -- https://github.com/nvim-telescope/telescope.nvim
+    'nvim-telescope/telescope.nvim',
+    lazy = true,
+    branch = '0.1.x',
+    dependencies = {
+        -- https://github.com/nvim-lua/plenary.nvim
+        { 'nvim-lua/plenary.nvim' },
+        {
+            -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+            cond = function()
+                return vim.fn.executable 'make' == 1
+            end,
+        },
     },
-  },
-  opts = {
-    defaults = {
-      layout_config = {
-        vertical = {
-          width = 0.75
-        }
-      },
-      mappings = {
-        i = {
-           ["<esc>"] = function(...) require("telescope.actions").close(...) end,
-          },
-      }
+    opts = {
+        defaults = {
+            layout_config = {
+                vertical = {
+                    width = 0.75
+                }
+            },
+            mappings = {
+                i = {
+                    ["<esc>"] = function(...) require("telescope.actions").close(...) end,
+                },
+            }
+        },
+        pickers = {
+            find_files = {
+                -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+            },
+        },
     }
-  }
 }
