2320063792ff9a6cb7fadc05255ea4193ec31af2
David Szotten <davidszotten@gmail.com>
2024-03-12 10:48:39 +0000

    disable trailing-whitespace

--- .config/nvim/lua/plugins/vim-trailing-whitespace.lua
+++ .config/nvim/lua/plugins/vim-trailing-whitespace.lua
@@ -1,7 +1,7 @@
 -- Show trailing whitespace
 return {
+    enabled = false,
     -- https://github.com/davidszotten/vim-trailing-whitespace
     'davidszotten/vim-trailing-whitespace',
     lazy = false,
 }
-
