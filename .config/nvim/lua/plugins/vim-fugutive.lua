8717122f39889c98f1fe19ea8b9d5bd6f4233a05
David Szotten <davidszotten@gmail.com>
2024-03-13 11:02:03 +0000

    fugutive config

--- .config/nvim/lua/plugins/vim-fugutive.lua
+++ .config/nvim/lua/plugins/vim-fugutive.lua
@@ -4,6 +4,10 @@ return {
     event = 'VeryLazy',
     dependencies = {
         'tpope/vim-rhubarb',
-    }
+    },
+    config = function()
+        -- set in local_options:
+        -- vim.g.github_enterprise_urls = { 'https://example.com' }
+        vim.api.nvim_create_autocmd({ "BufReadPost" }, { pattern = { "fugitive://*" }, command = "set bufhidden=delete" })
+    end
 }
-
